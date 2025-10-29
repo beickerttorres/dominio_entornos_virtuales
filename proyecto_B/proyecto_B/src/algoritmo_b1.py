4bcc031be26d226206e9c991afcf6b2408a571d8
beickert <beickerttapia@gmail.com>
2025-10-29 13:08:48 -0500

    Inicialización del repositorio con proyecto A y B

---
+++ proyecto_B/proyecto_B/src/algoritmo_b1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data = {'Nombre': ['Ana', 'Luis', 'María'], 'Edad': [23, 31, 29]}
+df = pd.DataFrame(data)
+print("Tabla de datos:\n", df)
---
+++ proyecto_B/proyecto_B/src/algoritmo_b2.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+numeros = [10, 20, 30, 40]
+serie = pd.Series(numeros)
+print("Promedio:", serie.mean())
